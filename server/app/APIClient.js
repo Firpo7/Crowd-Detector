89a067a2600c65bb3cd373f2d931e38c7d9e221d
Firpo7 <tizianofirpo@gmail.com>
2020-07-14 23:58:11 +0200

    [refactor] promisified functions

--- server/app/APIClient.js
+++ server/app/APIClient.js
@@ -37,89 +37,99 @@ function getListOf(value) {
   return ret
 }
 
-function getNodesFromDB(res, building, floors, types) {
-  let query = knex.select([knex.ref('public_id').as('id'), 'name', 'floor', 'roomtype']).from('sensor').where('building', building)
-  if (floors.length) query.whereIn('floor', floors)
-  if (types.length) query.whereIn('roomtype', types)
+function getNodesFromDB(building, ids=[], floors=[], types=[]) {
+  let toPromise = function( resolve, reject ) {
+    let query = knex.select([knex.ref('public_id').as('id'), 'name', 'floor', 'roomtype']).from('sensor')
+    if (building) query.where('building', building)
+    if (ids.length) query.whereIn('public_id', ids)
+    if (floors.length) query.whereIn('floor', floors)
+    if (types.length) query.whereIn('roomtype', types)
 
-  query.then((rows) => {
-    res.send({code: APIconstants.API_CODE_SUCCESS, listOfNodes: rows})
-  }).catch((err) => { res.send({ code: APIconstants.API_CODE_GENERAL_ERROR }); console.log(err) })
-}
-
-function getBuildingsFromDB(res, name) {
-  let query = knex.select().from('building')
-  if (name) query.where('name', name)
-  
-  query.then((rows) => {
-    res.send({code: APIconstants.API_CODE_SUCCESS, listOfNodes: rows})
-  }).catch((err) => { res.send({ code: APIconstants.API_CODE_GENERAL_ERROR }) })
-}
-
-function getSimpleStatisticsFromDB(res, listOfIdSensors) {
-  knex.select('sensor_id','current_people').from('sensor_data').whereIn('sensor_id', listOfIdSensors).orderBy('time','desc').limit(1).then((rows) => {
-    res.send({code: APIconstants.API_CODE_SUCCESS, datas: rows})
-  }).catch((err) => { res.send({ code: APIconstants.API_CODE_GENERAL_ERROR }); console.log(err) })
-}
-
-function getStatisticsFromDB(res, listOfIdSensors, operation, option_range) {
-  let dayColumn = knex.ref(knex.raw('concat( EXTRACT(MONTH FROM time), \'-\', EXTRACT(DAY FROM time))')).as('day')
-  let query = knex('sensor_data').whereIn('sensor_id', listOfIdSensors)
-  let operation_column
-
-  switch (operation) {
-    case OPERATIONS.AVG_NUMBER_OF_PEOPLE:
-      operation_column = knex.ref(knex.raw('AVG(current_people)')).as('result')
-      break;
-    
-    case OPERATIONS.MAX_NUMBER_OF_PEOPLE:
-      operation_column = knex.ref(knex.raw('MAX(current_people)')).as('result')
-      break;
-    
-    case OPERATIONS.NUMBER_OF_DISTICT_PEOPLE:
-      operation_column = knex.ref(knex.raw('SUM(new_people)')).as('result')
-      break;
-    
-    default:
-      res.send ({ code: APIconstants.API_CODE_INVALID_DATA });
-      return;
+    query.then((rows) => resolve(rows) )
+    .catch((err) => reject(err))
   }
+  return new Promise(toPromise)
+}
 
-  let current_date = new Date()
-  let today_date = new Date(Date.UTC(current_date.getUTCFullYear(), current_date.getUTCMonth(), current_date.getUTCDate()));
-
-  switch (option_range) {
-    case OPTION_RANGE.TODAY:
-      query.where('time', '>', today_date)
-      break;
+function getBuildingsFromDB(name) {
+  let toPromise = function( resolve, reject ) {
+    let query = knex.select().from('building')
+    if (name) query.where('name', name)
     
-    case OPTION_RANGE.YESTERDAY:
-      let yesterday_date = new Date(today_date.getTime())
-      yesterday_date.setDate(yesterday_date.getDate() - 1)
-      query.whereBetween('time', [yesterday_date, today_date])
-      break;
-    
-    case OPTION_RANGE.LAST_WEEK:
-      let lastweek_date = new Date(today_date.getTime())
-      lastweek_date.setDate(lastweek_date.getDate() - 7)
-      query.whereBetween('time', [lastweek_date, today_date])
-      break;
-    
-    case OPTION_RANGE.LAST_MONTH:
-      let lastmonth_date = new Date(today_date.getTime())
-      lastmonth_date.setDate(lastmonth_date.getDate() - 30)
-      query.whereBetween('time', [lastmonth_date, today_date])
-      break;
-    
-    default:
-      res.send ({ code: APIconstants.API_CODE_INVALID_DATA });
-      return;
+    query.then((rows) => resolve(rows))
+    .catch((err) => reject(err))
   }
+  return new Promise(toPromise)
+}
 
-  query.select(dayColumn, operation_column).groupBy('day').then((rows) => {
-    res.send({code: APIconstants.API_CODE_SUCCESS, listOfNodes: rows})
-  }).catch((err) => { res.send({ code: APIconstants.API_CODE_GENERAL_ERROR }); console.log(err) })
+function getSimpleStatisticsFromDB(listOfIdSensors=[]) {
+  let toPromise = function( resolve, reject ) {
+    knex.select('sensor_id','current_people').from('sensor_data').whereIn('sensor_id', listOfIdSensors).orderBy('time','desc').limit(1).then((rows) => {
+      resolve(rows)
+    }).catch((err) => reject(err))
+  }
+  return new Promise(toPromise)
+}
 
+function getStatisticsFromDB(listOfIdSensors, operation, option_range) {
+  let toPromise = function( resolve, reject ) {
+    let dayColumn = knex.ref(knex.raw('concat( EXTRACT(MONTH FROM time), \'-\', EXTRACT(DAY FROM time))')).as('day')
+    let query = knex('sensor_data').whereIn('sensor_id', listOfIdSensors)
+    let operation_column
+
+    switch (operation) {
+      case OPERATIONS.AVG_NUMBER_OF_PEOPLE:
+        operation_column = knex.ref(knex.raw('AVG(current_people)')).as('result')
+        break;
+      
+      case OPERATIONS.MAX_NUMBER_OF_PEOPLE:
+        operation_column = knex.ref(knex.raw('MAX(current_people)')).as('result')
+        break;
+      
+      case OPERATIONS.NUMBER_OF_DISTICT_PEOPLE:
+        operation_column = knex.ref(knex.raw('SUM(new_people)')).as('result')
+        break;
+      
+      default:
+        reject({ code: APIconstants.API_CODE_INVALID_DATA });
+        return;
+    }
+
+    let current_date = new Date()
+    let today_date = new Date(Date.UTC(current_date.getUTCFullYear(), current_date.getUTCMonth(), current_date.getUTCDate()));
+
+    switch (option_range) {
+      case OPTION_RANGE.TODAY:
+        query.where('time', '>', today_date)
+        break;
+      
+      case OPTION_RANGE.YESTERDAY:
+        let yesterday_date = new Date(today_date.getTime())
+        yesterday_date.setDate(yesterday_date.getDate() - 1)
+        query.whereBetween('time', [yesterday_date, today_date])
+        break;
+      
+      case OPTION_RANGE.LAST_WEEK:
+        let lastweek_date = new Date(today_date.getTime())
+        lastweek_date.setDate(lastweek_date.getDate() - 7)
+        query.whereBetween('time', [lastweek_date, today_date])
+        break;
+      
+      case OPTION_RANGE.LAST_MONTH:
+        let lastmonth_date = new Date(today_date.getTime())
+        lastmonth_date.setDate(lastmonth_date.getDate() - 30)
+        query.whereBetween('time', [lastmonth_date, today_date])
+        break;
+      
+      default:
+        reject({ code: APIconstants.API_CODE_INVALID_DATA });
+        return;
+    }
+
+    query.select(dayColumn, operation_column).groupBy('day').then((rows) => resolve(rows))
+    .catch((err) => reject(err))
+  }
+  return new Promise(toPromise)
 }
 
 function getNodesController(req, res) {
@@ -127,11 +137,15 @@ function getNodesController(req, res) {
   let floors = getListOf(req.query.floor)
   let types = getListOf(req.query.type)
 
-  getNodesFromDB(res, req.query.building, floors, types)
+  getNodesFromDB(building=req.query.building, floors=floors, types=types)
+  .then((rows) => res.send({code: APIconstants.API_CODE_SUCCESS, listOfNodes: rows}))
+  .catch((err) => {res.send({code:APIconstants.API_CODE_GENERAL_ERROR}) ; console.log(err) })
 }
 
 function getBuildingController(req, res) {
-  getBuildingsFromDB(res, req.query.name)
+  getBuildingsFromDB(req.query.name)
+  .then((rows) => res.send({code: APIconstants.API_CODE_SUCCESS, buildings: rows}))
+  .catch((err) => {res.send({code:APIconstants.API_CODE_GENERAL_ERROR}) ; console.log(err) })
 }
 
 function getStatisticsController(req, res) {
@@ -142,7 +156,9 @@ function getStatisticsController(req, res) {
         return
   }
   let ids = getListOf(req.query.id)
-  getStatisticsFromDB(res, ids, req.query.op.toLowerCase(), req.query.optionRange.toLowerCase())
+  getStatisticsFromDB(ids, req.query.op.toLowerCase(), req.query.optionRange.toLowerCase())
+  .then((rows) => res.send({code: APIconstants.API_CODE_SUCCESS, datas: rows}))
+  .catch((err) => {res.send({code: (err.code || APIconstants.API_CODE_GENERAL_ERROR)}) ; if (err.code) console.log(err) })
 }
 
 function getSimpleStatisticsController(req, res) {
@@ -151,7 +167,9 @@ function getSimpleStatisticsController(req, res) {
     return
   }
   let ids = getListOf(req.query.id)
-  getSimpleStatisticsFromDB(res, ids)
+  getSimpleStatisticsFromDB(ids)
+  .then((rows) => res.send({code: APIconstants.API_CODE_SUCCESS, datas: rows}))
+  .catch((err) => {res.send({code:APIconstants.API_CODE_GENERAL_ERROR}) ; console.log(err) })
 }
 
 exports.getSimpleStatistics = getSimpleStatisticsController
--- server/app/APIServer.js
+++ server/app/APIServer.js
@@ -3,7 +3,7 @@ const redis = require('redis')
 const redis_client = redis.createClient({
   port      : process.env.REDIS_PORT || 6379,
   host      : process.env.REDIS_HOST || 'localhost',
-  password  : process.env.REDIS_PASSWORD,
+  password  : process.env.REDIS_PASSWORD || undefined,
 });
 
 var APIconstants = require('./APIConstants').constants;
