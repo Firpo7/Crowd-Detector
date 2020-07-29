55849d68c8e70b4559256cd579c47ffb0ef71df7
Firpo7 <tizianofirpo@gmail.com>
2020-07-29 21:21:17 +0200

    [refactor] changed envvar SERVER_PORT to PORT

--- server/app/main.js
+++ server/app/main.js
@@ -6,7 +6,7 @@ const APIconstants = require('./Constants').APIConstants
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
-const port = process.env.SERVER_PORT || 3000
+const port = process.env.PORT || 3000
 
 function endpointLogger() {
   return (req, _, next) => {
