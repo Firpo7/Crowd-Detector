b026f85fc4c89e1140d973de9a5ac844ed9b00c3
A-725-K <andrea.canepa.12@gmail.com>
2020-07-29 15:13:17 +0200

    [feat] Finished simulator to fill db with sample data

--- raspberry/DIBRIS_simulator.js
+++ raspberry/DIBRIS_simulator.js
@@ -1,31 +1,53 @@
 'use strict';
 
 const fetch = require('node-fetch');
-const APIconstants = require('../server/app/APIConstants').APIConstants;
+const APIconstants = require('../server/app/Constants').APIConstants;
+const ROOMTYPES = Array.from(require('../server/app/Constants').ParamsConstants.ROOMTYPES.keys());
 
 const API = 'http://localhost:3000';
 //const API = 'https://iot-proj00.herokuapp.com';
 const DIBRIS = 'DIBRIS-VP';
 const TOKEN = 'AAAAABBBBBCCCCCDDDDDEEEEE';
 
-const ROOMTYPES = {
-    o: 'office',
-    lr: 'lecture room',
-    cr: 'common room',
-    l: 'library',
-    sr: 'study room',
-    lb: 'laboratory',
-    rr: 'reserved room'
+const MAX_PEOPLE_PER_ROOM = {
+    'Studying area': 50,
+    'Room 710': 90,
+    'Room 711': 70,
+    'Room 506': 100,
+    'Studying area': 50,
+    'Room 505': 60,
+    'Library': 10,
+    'Studying room': 75,
+    'Lab SW-2': 40,
+    'Conference room': 80,
+    'Office Prof X': 5,
+    'Office Prof Y': 5,
+    'Office Prof Z': 4,
+    'PhD Room': 30,
+    'Lab SW-1': 55,
+    'Room 216': 45,
+    'Room 217': 45,
+    'Room 218': 45,
+    'Office Prof A': 4,
+    'Office Prof B': 6,
+    'Office Prof C': 5,
+    'Office Prof D': 4,
+    'ZenHack Room': 7,
+    'Common room 1': 15,
+    'Common room 2': 40,
+    'Little kitchen': 12
 };
 
-const sensors_ids = [];
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
-function createSensorObj(name, floor, type, max_people) {
+function createSensorObj(name, floor, type) {
     let pieces = [
         'name=' + name,
         'floor=' + floor,
         'type=' + type,
-        'max_people=' + max_people,
+        'max_people=' + MAX_PEOPLE_PER_ROOM[name],
         'building=' + DIBRIS,
         'token=' + TOKEN
     ];
@@ -47,13 +69,14 @@ function createBuildingObj(name, numfloors, address) {
 function registerBuildings() {
     const buildingsToRegister = [
         createBuildingObj(DIBRIS, 8, 'via Dodecaneso'),
-        createBuildingObj('DIBRIS-OP', 6, 'via all\'Opera Pia'),
+        createBuildingObj('DIBRIS-OP', 6, 'via all Opera Pia'),
         createBuildingObj('CAMPUS SAVONA, PALAZZINA LAGORIO', 2, 'via Magliotto'),
         createBuildingObj('DISTAV', 7, 'corso Europa')
     ];
 
-    let toPromise = (resolve, reject) => {
-        buildingsToRegister.forEach(b => {
+    let promises = [];
+    buildingsToRegister.forEach(b => {
+        promises.push(
             fetch(API + APIconstants.API_ENDPOINT_REGISTER_NEW_BUILDING,
                 {
                     method: 'POST',
@@ -64,45 +87,44 @@ function registerBuildings() {
             .then(data => {
                 if (data.code !== 42) {
                     console.log('ERROR_B:', data.code, 'COULD NOT REGISTER ->', b);
-                    reject(data.code);
+                    return;
                 }
-                resolve();
             })
-            .catch(err => reject(err));
-        });
-    };
+            .catch(err => console.log(err))
+        );
+    });
     
-    return new Promise(toPromise);
+    return Promise.all(promises);
 }
 
 function registerSensorsAndGetPrivateIds() {
     const sensorsToRegister = [
-        createSensorObj('Studying area', 7, ROOMTYPES['sr'], 50),
-        createSensorObj('Room 710', 7, ROOMTYPES['lr'], 90),
-        createSensorObj('Room 711', 7, ROOMTYPES['lr'], 70),
-        createSensorObj('Room 506', 5, ROOMTYPES['lr'], 100),
-        createSensorObj('Studying area', 5, ROOMTYPES['sr'], 50),
-        createSensorObj('Room 505', 5, ROOMTYPES['ls'], 60),
-        createSensorObj('Library', 4, ROOMTYPES['l'], 10),
-        createSensorObj('Studying room', 4, ROOMTYPES['sr'], 75),
-        createSensorObj('Lab SW-2', 3, ROOMTYPES['lb'], 40),
-        createSensorObj('Conference room', 3, ROOMTYPES['rr'], 80),
-        createSensorObj('Office Prof X', 3, ROOMTYPES['o'], 5),
-        createSensorObj('Office Prof Y', 3, ROOMTYPES['o'], 5),
-        createSensorObj('Office Prof Z', 3, ROOMTYPES['o'], 4),
-        createSensorObj('PhD Room', 3, ROOMTYPES['o'], 30),
-        createSensorObj('Lab SW-1', 2, ROOMTYPES['lb'], 55),
-        createSensorObj('Room 216', 2, ROOMTYPES['lr'], 45),
-        createSensorObj('Room 217', 2, ROOMTYPES['lr'], 45),
-        createSensorObj('Room 218', 2, ROOMTYPES['lr'], 45),
-        createSensorObj('Office Prof A', 2, ROOMTYPES['o'], 4),
-        createSensorObj('Office Prof B', 2, ROOMTYPES['o'], 6),
-        createSensorObj('Office Prof C', 2, ROOMTYPES['o'], 5),
-        createSensorObj('Office Prof D', 2, ROOMTYPES['o'], 4),
-        createSensorObj('ZenHack Room', 2, ROOMTYPES['rr'], 7),
-        createSensorObj('Common room 1', 2, ROOMTYPES['cr'], 15),
-        createSensorObj('Common room 2', 2, ROOMTYPES['cr'], 40),
-        createSensorObj('Little kitchen', 2, ROOMTYPES['cr'], 12)
+        createSensorObj('Studying area', 7, ROOMTYPES[5]),
+        createSensorObj('Room 710', 7, ROOMTYPES[0]),
+        createSensorObj('Room 711', 7, ROOMTYPES[0]),
+        createSensorObj('Room 506', 5, ROOMTYPES[0]),
+        createSensorObj('Studying area', 5, ROOMTYPES[5]),
+        createSensorObj('Room 505', 5, ROOMTYPES[0]),
+        createSensorObj('Library', 4, ROOMTYPES[1]),
+        createSensorObj('Studying room', 4, ROOMTYPES[5]),
+        createSensorObj('Lab SW-2', 3, ROOMTYPES[6]),
+        createSensorObj('Conference room', 3, ROOMTYPES[4]),
+        createSensorObj('Office Prof X', 3, ROOMTYPES[2]),
+        createSensorObj('Office Prof Y', 3, ROOMTYPES[2]),
+        createSensorObj('Office Prof Z', 3, ROOMTYPES[2]),
+        createSensorObj('PhD Room', 3, ROOMTYPES[2]),
+        createSensorObj('Lab SW-1', 2, ROOMTYPES[6]),
+        createSensorObj('Room 216', 2, ROOMTYPES[0]),
+        createSensorObj('Room 217', 2, ROOMTYPES[0]),
+        createSensorObj('Room 218', 2, ROOMTYPES[0]),
+        createSensorObj('Office Prof A', 2, ROOMTYPES[2]),
+        createSensorObj('Office Prof B', 2, ROOMTYPES[2]),
+        createSensorObj('Office Prof C', 2, ROOMTYPES[2]),
+        createSensorObj('Office Prof D', 2, ROOMTYPES[2]),
+        createSensorObj('ZenHack Room', 2, ROOMTYPES[4]),
+        createSensorObj('Common room 1', 2, ROOMTYPES[3]),
+        createSensorObj('Common room 2', 2, ROOMTYPES[3]),
+        createSensorObj('Little kitchen', 2, ROOMTYPES[3])
     ];
 
     let promises = [];
@@ -120,7 +142,9 @@ function registerSensorsAndGetPrivateIds() {
                     console.log('ERROR_S:', data.code, 'COULD NOT REGISTER ->', s);
                     return;
                 }
-                sensors_ids.push(data.id);
+
+                const sensorName = s.split('=')[1].split('&')[0];
+                sensors_ids[sensorName] = data.id;
             })
             .catch(err => console.log(err))
         );
@@ -129,12 +153,75 @@ function registerSensorsAndGetPrivateIds() {
     return Promise.all(promises);
 }
 
+function updatePeopleCount(id, currP, newP, time) {
+    fetch(API + APIconstants.API_ENDPOINT_UPDATE_CROWD,
+            {
+                method: 'POST',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                body: 'id=' + id + '&current=' + currP + '&new=' + newP + 
+                      '&time=' + time
+            })
+            .then(res => res.json())
+            .then(data => {
+                if (data.code !== 42)
+                    console.log('ERROR:', data.code);
+            })
+            .catch(err => console.log(err));
+}
+
+function fillWithSimulatedData() {
+    const SW1 = 'Lab SW-1';
+    const LC  = 'Little kitchen';
+
+    let currPeople = Array(2);
+    let newPeople = Array(2);
+    let adjust = Array(2);
+
+    const yy = 2020;
+    const MM = 7;
+    for (let dd=1; dd<=31; dd++) {
+        currPeople[0] = getRandomInt(MAX_PEOPLE_PER_ROOM[SW1]);
+        currPeople[1] = getRandomInt(MAX_PEOPLE_PER_ROOM[LC]);
+        for (let hh=10; hh<21; hh++) {    //timetable DIBRIS: 8-19, UTC+2
+            for (let mm=0; mm<60; mm+=10) {
+                let coin = getRandomInt(100);
+                for (let i=0; i<adjust.length; i++)
+                    adjust[i] = parseInt(currPeople[i]/10) + (coin > 50 ? 1 : 0);
+                
+                currPeople[0] = 
+                    coin > 50 ? 
+                        (currPeople[0] + adjust[0] > MAX_PEOPLE_PER_ROOM[SW1] ? MAX_PEOPLE_PER_ROOM[SW1] : currPeople[0] + adjust[0]) :
+                        (currPeople[0] - adjust[0] < 0 ? 0 : currPeople[0] - adjust[0]);
+
+                currPeople[1] = 
+                    coin > 50 ? 
+                        (currPeople[1] + adjust[1] > MAX_PEOPLE_PER_ROOM[LC] ? MAX_PEOPLE_PER_ROOM[LC] : currPeople[1] + adjust[1]) :
+                        (currPeople[1] - adjust[1] < 0 ? 0 : currPeople[1] - adjust[1]);
+
+                for (let i=0; i<newPeople.length; i++)
+                    newPeople[i] = coin > 50 ? parseInt(currPeople[i]/10) : 0;
+
+                let time = new Date(`${MM}/${dd}/${yy} ${hh}:${mm}`).toISOString();
+
+                updatePeopleCount(sensors_ids[SW1], currPeople[0], newPeople[0], time);
+                updatePeopleCount(sensors_ids[LC], currPeople[1], newPeople[1], time);
+            }
+        }
+    }
+}
+
+// MAIN
+const sensors_ids = {};
+
 // START THE SIMULATION
 console.log('Registering buildings...');
 registerBuildings()
     .then(() => {
         console.log('Registering sensors...');
         registerSensorsAndGetPrivateIds()
-            .then(() => console.log(sensors_ids));
+            .then(() => {
+                console.log('Filling tables with simulated data...');
+                fillWithSimulatedData();
+            })
     })
     .catch(err => console.log(err));
